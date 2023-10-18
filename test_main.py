a6848d7dc7f1c970582735c7c07fbf7526865467
bakha <djickel.diaw@systematix-qc.com>
2023-10-17 21:08:33 -0400

    fix the method test_something

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ from main import ZeroPointTemperatureCalculator
 DATA = [20, 70, 50, 100]
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(True, True)  # add assertion here
 
     def test_calculate(self): #test calculator for pipeline
         calculator = ZeroPointTemperatureCalculator()
